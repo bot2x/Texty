17679f0b97d781fb98c13cd1c5aa2e9a64f26ca0
Shubham <shubham0921s@gmail.com>
2024-02-14 23:03:41 +0530

    adding the tablayout along with switching.

---
+++ js/primitive/textmass.js
@@ -0,0 +1,12 @@
+class Textmass {
+
+    constructor() {
+        //this needs to be a list of strings. Each element corresponds to a line.
+        this.textLines = [];
+    }
+
+    addLines (line) {
+        this.textLines.push(line);
+    }
+
+}
